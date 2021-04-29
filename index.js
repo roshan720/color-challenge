43ddf582ae4bc2ea311a759ea5259201850c9a34
roshan720 <63476520+roshan720@users.noreply.github.com>
2021-04-29 17:19:41 +0530

    used jquery

---
+++ index.js
@@ -0,0 +1,20 @@
+$(".r").click(function(){
+    $(".r").css("background-color","white")
+});
+$(".b").click(function(){
+    $(".b").css("background-color","white")
+    $('.container').on('click', function () { 
+        var obj = document.createElement("audio");
+            obj.src = "mi1.wav"; 
+            obj.play();
+});
+});
+$(".y").click(function(){
+    $(".y").css("background-color","white")
+});
+$(".g").click(function(){
+    $(".g").css("background-color","white")   
+});
+
+
+
